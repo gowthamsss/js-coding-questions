cb15e0af697061ea5d53c967bf22c4cbbecebf2b
Gowtham S <52155234+gowthamsss@users.noreply.github.com>
2024-10-01 13:49:04 +0530

    Update isPalindrome.js

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,10 +1,23 @@
-let str = "racecar";
+// let str = "racecar";
 
-const isPalindrome = (str) => {
-  //   str = str.toLowerCase();                      // to make all string value as lowercase if required
-  const result = str.split("").reverse().join("");
-  if (str === result)
-    return `${str} is a palindrome`; // template literal format using ES6 concept
+// const isPalindrome = (str) => {
+//   //   str = str.toLowerCase();                      // to make all string value as lowercase if required
+//   const result = str.split("").reverse().join("");
+//   if (str === result)
+//     return `${str} is a palindrome`; // template literal format using ES6 concept
+//   else return `${str} is not a palindrome`;
+// };
+// console.log(isPalindrome(str));
+
+let str = "madam";
+const reverseAStr = (str) => {
+  let reversedStr = "";
+  // str = str.toLowerCase();
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  if (str === reversedStr) return `${str} is a palindrome`;
   else return `${str} is not a palindrome`;
 };
-console.log(isPalindrome(str));
+
+console.log(reverseAStr(str));
