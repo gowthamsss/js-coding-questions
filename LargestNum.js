d441c7847b6c1f43c6202772e278a6daafc30bc2
Gowtham S <52155234+gowthamsss@users.noreply.github.com>
2024-10-01 17:58:28 +0530

    Update LargestNum.js

--- LargestNum.js
+++ LargestNum.js
@@ -1,6 +1,8 @@
-const arr = [100, 99, 82, 10, 1, 2, 40];
+let arr = [10, 99, 872, 100, 1, 2, 40];
+// let arr = ["10", "99", "882", "100", "1", "2", "40"];
 
 const toFindLarge = (arr) => {
+  arr = arr.map(Number); // if arr of strings is an input , map and convert it into Number
   let largest = arr[0];
   for (let i = 1; i < arr.length - 1; i++) {
     if (arr[i] > largest) {
